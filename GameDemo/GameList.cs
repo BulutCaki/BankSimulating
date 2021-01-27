78c8ef445e37a3402678ddadc7d63dfcd1752045
Bulut Caki <bulutcaki023@gmail.com>
2021-01-27 21:03:14 +0300

    GameSimulating

---
+++ GameDemo/GameList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameDemo
+{
+    class GameList<T>
+    {
+        T[] items;
+        T[] tempArray;
+
+        public GameList()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+
+        public int Length
+        {
+            get { return items.Length; }
+        }
+
+        public T[] Game
+        {
+            get { return items; }
+        }
+    }
+}
---
+++ GameDemo/UserManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameDemo
+{
+    class UserManager
+    {
+        public UserManager(IVerification verification)
+        {
+            verification.Edevlet();
+        }
+        public void Register(User user)
+        {
+            Console.WriteLine(user.UserName + " Kayıt İşlemi Başarıyla Gerçekleştirildi.");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Güncelleme İşlemi Başarıyla Gerçekleştirildi.");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("Silme İşlemi Başarıyla Gerçekleştirildi.");
+        }
+    }
+}
