316030062a90732def9805f213ce06939c1a8de9
BulutCaki <77459428+BulutCaki@users.noreply.github.com>
2021-01-27 21:07:58 +0300

    Add files via upload

---
+++ DiscountManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameDemo
+{
+    class DiscountManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Kampanya Eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Kampanya güncellendi.");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Kampanya Silindi.");
+        }
+    }
+        
+}
---
+++ User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameDemo
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string TcNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
